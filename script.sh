59dfebbda324c5716eceac8c7291a94e65f642f8
yokselzok <yokselzok@gmail.com>
2018-03-29 18:17:29 +0300

    Try bash

--- script.sh
+++ script.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
-pwd
-echo "NODE_ENV: $NODE_ENV"
+echo "Bash start -----------"
+echo "NODE_ENV: ($NODE_ENV)"
 echo "PRBUILD: $PRBUILD"
 echo "PRODFLAG: $PRODFLAG"
+
+if [ NODE_ENV == "production" ]
+then
+npm i --production;
+else
+npm i;
+npm run build;
+fi
+
+echo "Bash end -----------"
